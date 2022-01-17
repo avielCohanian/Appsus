8c4390f493113577826dd6f829eb840f953dc49d
Aviel Cohanian <avial151515@gmail.com>
2022-01-17 12:55:08 +0200

    home page

--- src/views/home.vue
+++ src/views/home.vue
@@ -1,14 +1,24 @@
 <template>
-  <div class="home">
+  <div
+    class="home"
+    :style="{
+      'background-image': 'url(' + img + ')',
+    }"
+  >
+    <div class="container">
+      <h1>Welcome To Appsus</h1>
+    </div>
   </div>
 </template>
 
 <script>
-
-export default {
-  name: 'home',
-  components: {
-    
-  }
-}
+  export default {
+    name: 'home',
+    data() {
+      return {
+        img: require('@/assets/img/bcimg.jpg'),
+      };
+    },
+    components: {},
+  };
 </script>
